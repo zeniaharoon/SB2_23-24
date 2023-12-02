0f232f91fcd3449d50657aab451533c122947793
yashp123456 <136022696+yashp123456@users.noreply.github.com>
2023-12-02 14:01:28 -0500

    Update capute_example.py

--- capute_example.py
+++ capute_example.py
@@ -8,110 +8,309 @@ import numpy as np
 
 import image_processing as ip
 
+use_camera = False
+path = "C:/Users/yashi/PycharmProjects/pythonProject1/"
+file_name = path + "Blue_center.png"
 
-# def nothing(x):
-#     pass
+def nothing(x):
+    pass
+
+def on_mouse_click (event, x, y, flags, frame):
+    if event == cv2.EVENT_LBUTTONUP:
+        print(frame[y,x].tolist())
 
 
 cv2.namedWindow('image')
+cv2.resizeWindow('image', 300, 400)
 
 # create trackbars for color change
-# cv2.createTrackbar('lowH', 'image', 0, 179, nothing)
-# cv2.createTrackbar('highH', 'image', 179, 179, nothing)
-#
-# cv2.createTrackbar('lowS', 'image', 0, 255, nothing)
-# cv2.createTrackbar('highS', 'image', 255, 255, nothing)
-#
-# cv2.createTrackbar('lowV', 'image', 0, 255, nothing)
-# cv2.createTrackbar('highV', 'image', 255, 255, nothing)
+cv2.createTrackbar('lowH', 'image', 0, 179, nothing)
+cv2.createTrackbar('highH', 'image', 179, 179, nothing)
 
+cv2.createTrackbar('lowS', 'image', 0, 255, nothing)
+cv2.createTrackbar('highS', 'image', 255, 255, nothing)
+
+cv2.createTrackbar('lowV', 'image', 0, 255, nothing)
+cv2.createTrackbar('highV', 'image', 255, 255, nothing)
+
+
+if True: #Blue
+    cv2.setTrackbarPos('lowH', 'image', 106)
+    cv2.setTrackbarPos('highH', 'image', 134)
+
+    cv2.setTrackbarPos('lowS', 'image', 124)
+    cv2.setTrackbarPos('highS', 'image', 255)
+
+    cv2.setTrackbarPos('lowV', 'image', 0)
+    cv2.setTrackbarPos('highV', 'image', 191)
+
+else: #red
+    cv2.setTrackbarPos('lowH', 'image', 153)
+    cv2.setTrackbarPos('highH', 'image', 179)
+
+    cv2.setTrackbarPos('lowS', 'image', 50)
+    cv2.setTrackbarPos('highS', 'image', 255)
+
+    cv2.setTrackbarPos('lowV', 'image', 20)
+    cv2.setTrackbarPos('highV', 'image', 255)
 
 # get current positions of the trackbars
 
 
-CAM_NUM = ip.findCamera()
+DO_QR_CODE_DETECTOR = False
+DO_COLOR_DETECTOR = True
+DO_ENABLE_SAVE_FRAMES = True
+DO_FIND_SQUARE = True
 
-if len(CAM_NUM) < 1:
-    print("Error: Unable to find capture device")
-    exit()
+CAM_NUM = 0
 
-# Assume first device
-CAM_NUM = CAM_NUM[0]
 
-cam = cv2.VideoCapture(CAM_NUM)
+def nothing(x):
+    pass
 
-if not cam.read()[0]:
-    print('Capture Failed')
-else:
-    print('Capture Not failed')
+
+def on_mouse_click(event, x, y, flags, frame):
+    if event == cv2.EVENT_LBUTTONUP:
+        colors.append(frame[y, x].tolist())
+
+
+# Instructions
+if DO_ENABLE_SAVE_FRAMES:
+    print('Press spacebar to save frame to disk')
+    img_counter = 0
+
+if DO_FIND_SQUARE:
+    print('SQUARE!!!')
+
+if DO_QR_CODE_DETECTOR:
+    print('Show the camera a QR code')
+
+if DO_COLOR_DETECTOR:
+    print('Press number keys to change color space')
+
+    COLOR_INDEX = 1
+    COLOR_MODE = ip.COLOR_LIST[COLOR_INDEX]
+    colors = []
+    MIN_CLICKS = 10
+
+    print('{} mouse clicks on upper left image will create '.format(MIN_CLICKS))
+
+if use_camera == True:
+    if CAM_NUM < 0:
+        CAM_NUM = ip.findCamera()
+
+        if len(CAM_NUM) < 1:
+            print("Error: Unable to find capture device")
+            exit()
+
+        # Assume first device
+        CAM_NUM = CAM_NUM[0]
+
+    cam = cv2.VideoCapture(CAM_NUM)
+
+    if not cam.read()[0]:
+        print('Capture Failed')
+    else:
+        print('Capture Not failed')
 
 while True:
-    # ilowH = cv2.getTrackbarPos('lowH', 'image')
-    low = [115, 125, 13]
+    ilowH = cv2.getTrackbarPos('lowH', 'image')
+    ihighH = cv2.getTrackbarPos('highH', 'image')
+    ilowS = cv2.getTrackbarPos('lowS', 'image')
+    ihighS = cv2.getTrackbarPos('highS', 'image')
+    ilowV = cv2.getTrackbarPos('lowV', 'image')
+    ihighV = cv2.getTrackbarPos('highV', 'image')
+    if use_camera:
+        ret, raw = cam.read()
+        imret, raw = cam.read()
+    else:
+        raw = cv2.imread(file_name)
+        ret = 1
 
-    # ihighH = cv2.getTrackbarPos('highH', 'image')
-    high = [150, 235, 211]
-    # ilowS = cv2.getTrackbarPos('lowS', 'image')
-    # ihighS = cv2.getTrackbarPos('highS', 'image')
-    # ilowV = cv2.getTrackbarPos('lowV', 'image')
-    # ihighV = cv2.getTrackbarPos('highV', 'image')
-    ret, raw = cam.read()
-    output = raw
+
     if not ret:
         print("failed to grab frame")
         break
+    output = raw.copy()
+    if DO_QR_CODE_DETECTOR:
+        ip.findQRCodes(raw, output)
 
-    # Create a window
+        cv2.imshow("test", output)
 
-    # convert color to hsv because it is easy to track colors in this color model
-    hsv = cv2.cvtColor(raw, cv2.COLOR_BGR2HSV)
-    lower_hsv = np.array(low)
-    higher_hsv = np.array(high)
-    # Apply the cv2.inrange method to create a mask
-    thresh = cv2.inRange(hsv, lower_hsv, higher_hsv)
-    # print(lower_hsv)
-    # print(higher_hsv)
+    if DO_FIND_SQUARE:
+        # findSquare(raw, output)
 
-    # hsv = cv2.cvtColor(raw, cv2.COLOR_BGR2HSV)
+        # Create a window
 
-    # create a binary thresholded image on hue between red and yellow
-    # lower = (90,20,10)
-    # upper = (255,115,100)
-    # thresh = cv2.inRange(hsv, lower, upper)
+        # convert color to hsv because it is easy to track colors in this color model
+        hsv = cv2.cvtColor(raw, cv2.COLOR_BGR2HSV)
+        lower_hsv = np.array([ilowH, ilowS, ilowV])
+        higher_hsv = np.array([ihighH, ihighS, ihighV])
+        # Apply the cv2.inrange method to create a mask
+        thresh = cv2.inRange(hsv, lower_hsv, higher_hsv)
+        # print(lower_hsv)
+        # print(higher_hsv)
 
-    # apply morphology
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
-    clean = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-    clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+        # hsv = cv2.cvtColor(raw, cv2.COLOR_BGR2HSV)
 
-    # get external contours
-    contours = cv2.findContours(clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
+        # create a binary thresholded image on hue between red and yellow
+        # lower = (90,20,10)
+        # upper = (255,115,100)
+        # thresh = cv2.inRange(hsv, lower, upper)
 
-    for c in contours:
-        # get rotated rectangle from contour
-        rot_rect = cv2.minAreaRect(c)
-        box = cv2.boxPoints(rot_rect)
-        box = np.int0(box)
-        # draw rotated rectangle on copy of img
-        cv2.drawContours(output, [box], 0, (0, 0, 0), 2)
+        # apply morphology
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+        clean = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+        clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-        # cv2.imshow("test", output)
+        # get external contours
+        contours = cv2.findContours(clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = contours[0] if len(contours) == 2 else contours[1]
+        contourAreas = []
+        for c in contours:
+            # get rotated rectangle from contour
+            rot_rect = cv2.minAreaRect(c)
+            box = cv2.boxPoints(rot_rect)
+            box = np.int0(box)
+            # draw rotated rectangle on copy of img
+            cv2.drawContours(output, [box], 0, (0, 0, 0), 2)
+            contourAreas.append(cv2.contourArea(c))
 
-    # if k % 256 == 27:
-    #     # ESC pressed
-    #     print("Escape hit, closing...")
-    #     break
-    #
-    # elif k % 256 == 32 and DO_ENABLE_SAVE_FRAMES:
-    #     # SPACE pressed
-    #     img_name = "opencv_frame_{}.png".format(img_counter)
-    #     cv2.imwrite(img_name, raw)
-    #     print("{} written!".format(img_name))
-    #     img_counter += 1
+        if len(contours) > 0:
+            max_index = contourAreas.index(max(contourAreas))
+            rot_rect = cv2.minAreaRect(contours[max_index])
+            box = cv2.boxPoints(rot_rect)
+            box = np.int0(box)
+            cv2.drawContours(output, [box], 0, (255, 0, 0), 2)
+            leftTh = 200
+            rightTh = 475
+            leftCounter = 0
+            rightCounter = 0
+
+            for b in box:
+
+                if b[0] < leftTh:
+                    leftCounter += 1
+
+                elif b[0] > rightTh:
+                    rightCounter += 1
+            line_thickness = 2
+            cv2.line(output, (leftTh, 0), (leftTh, 450), (0, 255, 0), thickness=line_thickness)
+            cv2.line(output, (rightTh, 0), (rightTh, 450), (0, 255, 0), thickness=line_thickness)
+
+            if leftCounter == 4:
+                # print("Box is in the left.")
+                output = ip.makeLabeledDisplayImage(output, "left")
+
+            elif rightCounter == 4:
+                # print("Box is in the right.")
+                output = ip.makeLabeledDisplayImage(output, "right")
+
+            elif leftCounter == 0 and rightCounter == 0:
+                # print("Box is in the center.")
+                output = ip.makeLabeledDisplayImage(output, "center")
+
+            else:
+                # print("Can't determine where box is.")
+                output = ip.makeLabeledDisplayImage(output, "unknown")
+
+        cv2.imshow("test", output)
+        cv2.setMouseCallback("test", on_mouse_click, output)
+
+
+    if DO_COLOR_DETECTOR:
+
+        # Convert image to selected color space
+        new = cv2.cvtColor(raw, COLOR_MODE["CV"])
+
+        if len(colors) > MIN_CLICKS:
+            m = np.mean(np.array(colors), 0)
+            s = np.std(np.array(colors), 0)
+
+            n = 3
+            low = (m - n * s)
+            high = (m + n * s)
+
+            # Threshold image
+            bw = cv2.inRange(new, low, high)
+
+            #
+            kernel = np.ones((9, 9), np.uint8)
+            bw00 = cv2.morphologyEx(bw, cv2.MORPH_OPEN, kernel)
+            bw01 = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+            bw11 = cv2.morphologyEx(bw, cv2.MORPH_ERODE, kernel)
+            bw10 = cv2.morphologyEx(bw, cv2.MORPH_DILATE, kernel)
+
+            disp_bw = cv2.vconcat([cv2.hconcat([bw00, bw01]), cv2.hconcat([bw10, bw11])])
+
+            aspect_ratio = disp_bw.shape[0] / disp_bw.shape[1]
+            max_width = 800
+            new_size = (max_width, int(max_width * aspect_ratio))
+
+            disp_img_new = cv2.resize(disp_img, new_size, interpolation=cv2.INTER_NEAREST)
+            cv2.imshow('bw', disp_img_new)
+
+        # else:
+        # if cv2.getWindowProperty("bw", 0) >= 0:
+        #    cv2.destroyWindow("bw")
+
+        # Make a panel of the images to show color channels
+        disp_img_rgb = cv2.hconcat([ip.makeLabeledDisplayImage(raw.copy(), "BGR"),
+                                    ip.makeLabeledDisplayImage(raw[:, :, 0],
+                                                               "Blue " + ip.formatMinMaxString(raw[:, :, 0])),
+                                    ip.makeLabeledDisplayImage(raw[:, :, 1],
+                                                               "Green " + ip.formatMinMaxString(raw[:, :, 1])),
+                                    ip.makeLabeledDisplayImage(raw[:, :, 2],
+                                                               "Red " + ip.formatMinMaxString(raw[:, :, 2]))])
+
+        disp_img_new = cv2.hconcat([ip.makeLabeledDisplayImage(new.copy(), COLOR_MODE["Name"]),
+                                    ip.makeLabeledDisplayImage(new[:, :, 0],
+                                                               COLOR_MODE["C"][0] + " " + ip.formatMinMaxString(
+                                                                   new[:, :, 0])),
+                                    ip.makeLabeledDisplayImage(new[:, :, 1],
+                                                               COLOR_MODE["C"][1] + " " + ip.formatMinMaxString(
+                                                                   new[:, :, 1])),
+                                    ip.makeLabeledDisplayImage(new[:, :, 2],
+                                                               COLOR_MODE["C"][2] + " " + ip.formatMinMaxString(
+                                                                   new[:, :, 2]))])
+
+        disp_img = cv2.vconcat([disp_img_rgb, disp_img_new])
+
+        aspect_ratio = disp_img.shape[0] / disp_img.shape[1]
+        max_width = 1200
+        new_size = (max_width, int(max_width * aspect_ratio))
+
+        disp_img_new = cv2.resize(disp_img, new_size, interpolation=cv2.INTER_NEAREST)
+        cv2.imshow('Color Panels', disp_img_new)
+
+        cv2.setMouseCallback('Color Panels', on_mouse_click, disp_img_new)
+
+    # Keyboard callbacks
+    k = cv2.waitKey(1)
+
+    if k % 256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+
+    elif k % 256 == 32 and DO_ENABLE_SAVE_FRAMES:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, raw)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+    elif DO_COLOR_DETECTOR:
+        idx = k - 48
+        if idx > 0 and idx <= len(ip.COLOR_LIST):
+            colors.clear()
+            COLOR_INDEX = idx - 1
+            COLOR_MODE = ip.COLOR_LIST[COLOR_INDEX]
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+print(colors)
 
 cam.release()
